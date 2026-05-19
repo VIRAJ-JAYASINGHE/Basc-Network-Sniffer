26e7c58041babaa8e069b82250fac3274d423d46
VIRAJ-JAYASINGHE <isurujayasinghe1225@gmail.com>
2026-05-19 12:00:50 +0530

    import time,fuction call for loop add

--- Paket_send_data/Send_paket.py
+++ Paket_send_data/Send_paket.py
@@ -1,5 +1,5 @@
 from scapy.all import IP,TCP,ICMP, send  
-
+import time
 
 Target_ip = "10.54.22.117"
 
@@ -12,11 +12,28 @@ def send_test_pakect():
         icmp_paket = IP(dst=Target_ip) / ICMP() / "Viraj jayasinghe"
         send(icmp_paket, verbose=False)
 
+        print(f"send_data: viraj jayasinghe")
         time.sleep(1)
 
         #tcp paket box
 
         tcp_paket = IP(dst=Target_ip) / TCP(dport = 80) / "Password_1234"
+        send(Target_ip,verbose = False)
+
+        print(f"send_data: Password_1234")
+
+
+    except Exception as e:
+
+        print(f"Error sending pakets {e}")
+
+if __name__ == "__main__":
+
+    for i in range(2):
+        send_test_pakect()
+        time.sleep(2)
+
+
         
     
         
