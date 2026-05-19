6bfd7bd1379393d92814acaf6796bf643e30673f
VIRAJ-JAYASINGHE <isurujayasinghe1225@gmail.com>
2026-05-19 11:40:51 +0530

    creat ip layer,icmp protocol layer  and data part add

--- Paket_send_data/Send_paket.py
+++ Paket_send_data/Send_paket.py
@@ -1,1 +1,15 @@
-from scapy.all import Ip,TCP,ICMP, send  
+from scapy.all import IP,TCP,ICMP, send  
+
+
+Target_ip = "10.54.22.117"
+
+def send_test_pakect():
+    
+    print(f"sen paket to {10.54.22.117}")
+
+    try:
+
+        icmp_paket = IP(dst=Target_ip) / ICMP() / "Viraj jayasinghe"
+        
+
+
