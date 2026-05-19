8f1754b4161d21fd137fe5e5df8fd39e954b86ab
VIRAJ-JAYASINGHE <isurujayasinghe1225@gmail.com>
2026-05-19 11:43:39 +0530

    paket send and silend mode add

--- Paket_send_data/Send_paket.py
+++ Paket_send_data/Send_paket.py
@@ -4,12 +4,13 @@ from scapy.all import IP,TCP,ICMP, send
 Target_ip = "10.54.22.117"
 
 def send_test_pakect():
-    
+
     print(f"sen paket to {10.54.22.117}")
 
     try:
 
         icmp_paket = IP(dst=Target_ip) / ICMP() / "Viraj jayasinghe"
+        send(icmp_paket, verbose=False)
         
 
 
