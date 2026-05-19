3053fe248fe92d692e7aac8a38584cd1f5cca4c1
VIRAJ-JAYASINGHE <isurujayasinghe1225@gmail.com>
2026-05-19 11:50:01 +0530

    creat ip layer,tcp protocol layer,add port and data part add

--- Paket_send_data/Send_paket.py
+++ Paket_send_data/Send_paket.py
@@ -8,9 +8,17 @@ def send_test_pakect():
     print(f"sen paket to {10.54.22.117}")
 
     try:
-
+        #icmp paket box
         icmp_paket = IP(dst=Target_ip) / ICMP() / "Viraj jayasinghe"
         send(icmp_paket, verbose=False)
+
+        time.sleep(1)
+
+        #tcp paket box
+
+        tcp_paket = IP(dst=Target_ip) / TCP(dport = 80) / "Password_1234"
+        
+    
         
 
 
